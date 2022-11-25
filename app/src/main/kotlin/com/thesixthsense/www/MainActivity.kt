9e837dfc5d9924b756698ce7a20f37c022b8571c
apple <fahadyousafxi>
2022-11-25 10:52:04 +0500

    its first commit

---
+++ app/src/main/kotlin/com/thesixthsense/www/MainActivity.kt
@@ -0,0 +1,13 @@
+/*
+ * File name: MainActivity.kt
+ * Last modified: 2022.02.13 at 11:18:47
+ * Author: SmarterVision - https://codecanyon.net/user/smartervision
+ * Copyright (c) 2022
+ */
+
+package com.thesixthsense.www
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
